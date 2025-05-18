bf57ac4b4df16099fc19ca8dc596bfe4b19eedfc
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2025-05-18 09:04:26 +0000

    描述你的修改内容

--- app/app.py
+++ app/app.py
@@ -4,15 +4,6 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    try:
-        subprocess.Popen(["gotty", "-w", "-p", "3000", "bash", "terminal.sh"])
-        return jsonify({"status": "Gotty started"}), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-  
-
 @app.route('/api/data')
 def data():
     return jsonify({"status": "ok", "data": [1,2,3]})
