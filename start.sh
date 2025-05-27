77f63a6f8f425af1a0576be13a0ca3aa0e9a43d0
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2025-05-27 05:24:43 +0000

    OK

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ service php8.1-fpm start
 # 启动Flask应用（使用gunicorn）
 #cd /var/www/html/flaskapp && gunicorn -b 0.0.0.0:8000 app:app &
 #cd /var/www/html/flaskapp && gunicorn -b 0.0.0.0:8000 -k eventlet app:app &
-cd /var/www/html/flaskapp && python app.py &
+cd /var/www/html/flaskapp && python3 app.py &
 # 以appuser用户启动GoTTY
 su - root -c "gotty --permit-write --port 3000 bash" &
 
