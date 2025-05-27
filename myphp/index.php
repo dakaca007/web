e00e94a7f0b8fefcd2db379772301a2278d80e64
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2025-05-27 06:31:32 +0000

    OK

--- myphp/index.php
+++ myphp/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>实时聊天室</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.js"></script>
     <style>
